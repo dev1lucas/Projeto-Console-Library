555901392f267e2c22c009b3da2086bb5c6ac359
devlucas_s <105252290+dev1lucas@users.noreply.github.com>
2025-04-28 18:06:12 -0300

    Update Program.cs

--- ConsoleLibrary/Program.cs
+++ ConsoleLibrary/Program.cs
@@ -1,11 +1,9 @@
 ﻿using ConsoleLibrary.Modelos;
 
 var book = new Book("","");
-var bookShelf = new BookShelf(null);  // ainda não temos o menu
+var bookShelf = new BookShelf(null); 
 var menu = new Menu(bookShelf);
 
-// Agora corrigimos:
 bookShelf = new BookShelf(menu);
 
-// E chamamos o menu:
 menu.SetupMenu();
